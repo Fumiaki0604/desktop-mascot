ec251d6d588c2feefd808e7a4562dfe8c2bb4b65
Fumiaki0604 <noelnoel1003@yahoo.co.jp>
2025-10-14 19:21:50 +0900

    Fix: プロジェクトファイル設定を修正し、最新版が起動するように改善
    - DesktopMascotEnhanced.csを使用（最新の全コードを含む）
    - 古い分割ファイル（Program.cs, MainForm.cs等）を除外
    - start_mascot.batでプロジェクトファイルを明示的に指定
    - Newtonsoft.Jsonパッケージを追加

--- start_mascot.bat
+++ start_mascot.bat
@@ -1,5 +1,5 @@
 @echo off
 cd /d "C:\DesktopMascot_Enhanced"
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat"
-dotnet run
+dotnet run --project DesktopMascotEnhanced.csproj
 pause
